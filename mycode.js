2857419dde6191bc8e955a987755061701fd8e32
Rafael Deleuze <rafael.deleuze@gmail.com>
2023-08-06 01:28:58 +0200

    Remove bootstrap
    Now we are even more vanilla js.
    And when the music stops the button get unselected.
    and to stop the music, just click again

--- mycode.js
+++ mycode.js
@@ -1,10 +1,10 @@
 
 class Bt {
-    constructor(label, sound) {
-      this.label = label;
-      this.sound = sound;
-    }
+  constructor(label, sound) {
+    this.label = label;
+    this.sound = sound;
   }
+}
 
 var global_buttons = [
   new Bt("We build this city", "build_city.mp3"),
@@ -15,7 +15,7 @@ var global_buttons = [
   new Bt("Thick as a brick", "thick_as_a_brick.mp3"),
   new Bt("We will rock you", "rock.mp3"),
   new Bt("Whool?", "sheep.mp3"),
-  new Bt("I'm a wood", "wood.mp3"),
+  new Bt("Piece of wood", "piece_of_wood.mp3"),
   new Bt("Rolling in the deep", "rolling.mp3"),
   new Bt("Jeopardy", "jeopardy.mp3"),
   new Bt("What do you want?", "wannabe.mp3"),
@@ -25,11 +25,14 @@ var global_buttons = [
 function create_buttons() {
   t = document.getElementById("main_div");
 
-  for (let b = 0; b < global_buttons.length ; b++) {
-    let tr = document.createElement("button")
-    tr.setAttribute("class", "btn btn-outline-primary");
-    tr.innerHTML =  global_buttons[b].label;
+  for (let b = 0; b < global_buttons.length; b++) {
+    let tr = document.createElement("div")
+    tr.setAttribute("class", "btn");
+    tr.setAttribute("id", "div_" + global_buttons[b].sound)
 
+    let sp = document.createElement("span");
+    sp.innerHTML = global_buttons[b].label;
+    tr.appendChild(sp);
     tr.setAttribute("onclick", "on_click_play('" + global_buttons[b].sound + "')");
 
     t.appendChild(tr)
@@ -38,22 +41,49 @@ function create_buttons() {
 
 
 function on_click_play(sound) {
-  let ding = new Audio('sound/' + sound);
-  ding.setAttribute("id", sound);
-  document.body.appendChild(ding);
-  ding.addEventListener('ended', (event) => {
-    document.getElementById(sound).remove()
-  });
-  ding.play();
+  var current_play = who_is_playing();
+  on_stop_all();
+  if (sound != current_play) {
+    let ding = new Audio('sound/' + sound);
+    ding.setAttribute("id", sound);
+    document.body.appendChild(ding);
+    ding.addEventListener('ended', (event) => {
+      document.getElementById(sound).remove();
+      unselect_btn(sound);
+    });
+    ding.play();
+
+    let div = document.getElementById("div_" + sound);
+    div.style.backgroundColor = "lightskyblue"
+  }
+  else {
+    unselect_btn(current_play);
+  }
 }
 
+function who_is_playing() {
+  var audio_elts = document.querySelectorAll("audio");
+  if (audio_elts.length > 0) {
+    return audio_elts[0].id;
+  }
+  return "";
+}
+
+function unselect_btn(sound_name) {
+  let div = document.getElementById("div_" + sound_name);
+  div.style.backgroundColor = ""
+
+}
 
 function on_stop_all() {
   var audio_elts = document.querySelectorAll("audio");
-  audio_elts.forEach ( elt => {
+  audio_elts.forEach(elt => {
     elt.pause();
     elt.currentTime = 0;
     elt.remove();
+
+    let div = document.getElementById("div_" + elt.id);
+    div.style.backgroundColor = ""
   });
 }
 
